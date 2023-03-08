4279ab2b6845822510270363118a6d3b1b123986
Brian Brenner <bbrenne6@kent.edu>
2023-03-07 21:47:38 -0500

    rm purge

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.tsx"],
-  purge: {
-    content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.tsx}"]
-    // These options are passed through directly to PurgeCSS
-  },
   theme: {
     extend: {},
   },
