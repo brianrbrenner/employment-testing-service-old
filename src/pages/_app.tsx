91ada8740448123858bee83bb3fbd059f4535dd8
Brian Brenner <bbrenne6@kent.edu>
2023-03-08 12:03:01 -0500

    fixed directory

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,8 @@ import '../styles/globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import { api } from '../utils/api';
 import { type AppProps } from 'next/app';
-import { Header } from '../../components/layout/header';
-import { Layout } from '../../components/layout/layout';
+import { Header } from '../components/layout/header';
+import { Layout } from '../components/layout/layout';
 
 function App({ Component, pageProps }: AppProps) {
   return (
