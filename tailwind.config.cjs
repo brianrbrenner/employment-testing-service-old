834b7e91abeb5c62154f8111bb84bcd983b3917d
Brian Brenner <bbrenne6@kent.edu>
2023-03-07 20:59:10 -0500

    again

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.tsx"],
+  purge: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.tsx"],
   theme: {
     extend: {},
   },
